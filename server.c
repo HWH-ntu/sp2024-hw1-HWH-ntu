f4657f7c6d911436d90dfb4ecb670cd03f4a7ba4
HW. Hsu <r12945071@ntu.edu.tw>
2024-10-06 11:54:42 +0800

    5.1 (2): Modify print_train_info func_9: Debug format:try to figure out the end of file'?':msg_len

--- server.c
+++ server.c
@@ -103,7 +103,7 @@ int print_train_info(int train_fd, char* seat_availability_msg, size_t msg_len) 
     // Now, format the seat availability message (as before)
     memset(seat_availability_msg, 0, msg_len);  // Clear the buffer
     // Directly copy the raw content from seat_buffer to seat_availability_msg
-    snprintf(seat_availability_msg, msg_len-2, "%s", seat_buffer); 
+    snprintf(seat_availability_msg, msg_len, "%s", seat_buffer); 
     // 這邊msg_len-1因為發現最後有多一個問號，不確定是原本的string讀到什麼，所以只讓他讀到-1的位置
 
     return 0;  // Success
